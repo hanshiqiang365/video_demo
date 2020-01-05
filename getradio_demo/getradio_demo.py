ffd47622904047f935dd1936d2fa2a671cbeac38
hanshiqiang365 <shiqiang.han@accenture.com>
2020-01-05 23:30:14 +0800

    Add files via upload

---
+++ getradio_demo/getradio_demo.py
@@ -0,0 +1,13 @@
+﻿#author: hanshiqiang365 （微信公众号：韩思工作室）
+
+import os
+from ffmpy3 import FFmpeg
+
+changefile = 'Sakura_Draw.mp4'
+outputfile = 'Sakura_Draw.mp4.wav'
+
+ff = FFmpeg(inputs={changefile: None},outputs={outputfile: '-vn -ar 44100 -ac 2 -ab 192 -f wav'})
+print(ff.cmd)
+ff.run()
+
+        
